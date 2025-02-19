08fb987481aaaa2af4691717a680d4df0fb657fd
conniemyo <connieyoungxo@gmail.com>
2025-02-19 13:15:58 -0600

    added notes

--- app.js
+++ app.js
@@ -3,20 +3,22 @@ const counterValueElement = document.getElementById("counterValue");
 
 function updateCounterDisplay() {
     counterValueElement.textContent = counter;
-
+    // change based on variable value
     if (counter < 0) {
         counterValueElement.style.backgroundColor = "red";
     } else if (counter === 0) {
         counterValueElement.style.backgroundColor = "white";
-    } else {
+    } else { // change css with js
         counterValueElement.style.backgroundColor = "green";
     }
 }
 
 function incrementCounter() {
+    // for loop
     for (let i = 0; i < 1; i++) {
         counter++;
     }
+    // while loop
     while (counter < 10) {
         updateCounterDisplay();
         break;
