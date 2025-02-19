028432cc1e11639e2c0add05bac82ce8a137133c
conniemyo <connieyoungxo@gmail.com>
2025-02-19 13:09:25 -0600

    complete redo

--- app.js
+++ app.js
@@ -1,44 +1,39 @@
-var colors = ["red", "blue", "green", "yellow"];
-var box = document.getElementById("box");
-var currentColorIndex = 0;
-var themeMode = "light"; 
+let counter = 0;
+const counterValueElement = document.getElementById("counterValue");
 
-function changeBoxColor() {
-  for (var i = 0; i < colors.length; i++) {x
-    box.style.backgroundColor = colors[i];
-  }
-  
-  currentColorIndex++;
-  if (currentColorIndex >= colors.length) {
-    currentColorIndex = 0;
-  }
+function updateCounterDisplay() {
+    counterValueElement.textContent = counter;
+
+    // Change background color based on counter value
+    if (counter < 0) {
+        counterValueElement.style.backgroundColor = "red";
+    } else if (counter === 0) {
+        counterValueElement.style.backgroundColor = "white";
+    } else {
+        counterValueElement.style.backgroundColor = "green";
+    }
 }
 
-function resetBoxColor() {
-  var i = 0;
-  while (i < 1) {
-    box.style.backgroundColor = "gray";
-    i++;
-  }
+function incrementCounter() {
+    for (let i = 0; i < 1; i++) {
+        counter++;
+    }
+    while (counter < 10) {
+        updateCounterDisplay();
+        break;
+    }
+    updateCounterDisplay();
 }
 
-function changeTheme() {
-  console.log('Current theme:', themeMode);
-  if (themeMode === "light") {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-    box.style.backgroundColor = "blue";
-  } else {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
-    box.style.backgroundColor = "gray";
-  }
+function decrementCounter() {
+    for (let i = 0; i < 1; i++) {
+        counter--;
+    }
+    while (counter > -10) {
+        updateCounterDisplay();
+        break;
+    }
+    updateCounterDisplay();
 }
 
-document.getElementById("changeBtn").addEventListener("click", changeBoxColor);
-document.getElementById("resetBtn").addEventListener("click", resetBoxColor);
-document.getElementById("themeBtn").addEventListener("click", function() {
-  themeMode = (themeMode === "light") ? "dark" : "light";
-  console.log('Theme changed to:', themeMode);
-  changeTheme();
-});
+updateCounterDisplay();
